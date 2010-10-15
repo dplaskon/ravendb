9a22f77f692b7de226782c1c4d8f6c116a2f0ab0
Christopher Bennage <christopher@bennage.com>
2010-10-15 17:58:48 -0400

    partial work on Indexes and Search, knocking off for the weekend

--- RavenAdmin/SearchViewModel.cs
+++ RavenAdmin/SearchViewModel.cs
@@ -2,8 +2,8 @@
 {
 	using System.ComponentModel.Composition;
 
-	[Export(typeof(SummaryViewModel))]
-	public class SummaryViewModel
+	[Export(typeof(SearchViewModel))]
+	public class SearchViewModel
 	{
 	}
 }
